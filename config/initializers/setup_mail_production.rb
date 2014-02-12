23841dedd49725a95ab574ba000dc772ff80a53b
Gunn <pixeloon@gmail.com>
2014-02-11 22:19:38 -0800

    fixed mail production

--- config/initializers/setup_mail_production.rb
+++ config/initializers/setup_mail_production.rb
@@ -1,11 +1,11 @@
-if Rails.env.development?
+if Rails.env.production?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV["app21662567@heroku.com"],
-    :password       => ENV["5uasqcsy"],
+    :user_name      => ENV['app21662567@heroku.com'],
+    :password       => ENV['5uasqcsy'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
