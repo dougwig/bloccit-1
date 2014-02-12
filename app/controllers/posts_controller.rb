323d295af5ccbbdae41416bf15fdc5256e322182
Gunn <pixeloon@gmail.com>
2014-02-11 20:55:53 -0800

    Associated and nested topics and posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,21 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
+
 
   def show
     @post = Post.find(params[:id])
+    @topic = Topic.find(params[:topic_id])
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
     @post =  Post.new
     authorize! :create, Post, message: "You need to be a member to create a new post."
   end
 
   def create
+    @topic = Topic.find(params[:topic_id])
     @post = current_user.posts.build(params[:post])
+    @post.topic = @topic
 #    @post = Post.new(params[:post])
     #puts "POSTS CREATE = #{params.inspect}"
     #puts "CURRENT USER = #{current_user.email}"
@@ -23,8 +25,9 @@ class PostsController < ApplicationController
     #puts "POST CREATE, WE SHOULD NEVER SEE THIS."
     #raise # this will short-circuit the method
     if @post.save
-      flash[:notice] =  "Post was saved."
-      redirect_to @post
+      redirect_to [@topic,@post], notice: "Post was saved successfully"
+      #flash[:notice] =  "Post was saved."
+      #redirect_to @post
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :new
@@ -32,16 +35,19 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize! :edit, @post, message: "You need to own the post to edit it."
   end
 
   def update
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize! :update, @post, message: "You need to own the post to edit it."
     if @post.update_attributes(params[:post])
-      flash[:notice] = "Post was updated."
-      redirect_to @post
+      redirect_to [@topic,@post], notice: "Post was saved successfully"
+      #flash[:notice] = "Post was updated."
+      #redirect_to @post
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :edit
