3450d0fdc23364461939a183c1fcf4d91eafb571
Gunn <pixeloon@gmail.com>
2014-02-13 20:04:25 -0800

    sendgrid tweak

--- config/initializers/setup_mail_production.rb
+++ config/initializers/setup_mail_production.rb
@@ -1,12 +1,12 @@
-if Rails.env.production?
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['app21662567@heroku.com'],
-    :password       => ENV['5uasqcsy'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
-end
+# if Rails.env.production?
+#   ActionMailer::Base.delivery_method = :smtp
+#   ActionMailer::Base.smtp_settings = {
+#     :address        => 'smtp.sendgrid.net',
+#     :port           => '587',
+#     :authentication => :plain,
+#     :user_name      => ENV[SENDGRID_USERNAME],
+#     :password       => ENV[SENDGRID_PASSWORD],
+#     :domain         => 'heroku.com',
+#     :enable_starttls_auto => true
+#   }
+# end
